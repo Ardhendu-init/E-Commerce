98c57c3e04277b68ecc0beec49f5da41a97c41cb
Ardhendu Pramanik <ardhendujj002@gmail.com>
2022-05-13 23:15:34 +0530

    Added new component Slider and modified readme file

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { ShoppingCartOutlined, Search } from "@material-ui/icons";
 import React from "react";
 import styled from "styled-components";
-import { Badge } from "@material-ui/core";
+import Badge from "@mui/material/Badge";
 
 const Container = styled.div`
   height: 60px;
---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "./images/slide1.png",
+    title: "SUMMER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "./images/slide2.png",
+    title: "Indian Traditional",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "./images/slide3.png",
+    title: "Formal Men",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fbf0f4",
+  },
+];
