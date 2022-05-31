ad819683eccdcdea1d26132d54064799b235fe4f
Ardhendu Pramanik <ardhendujj002@gmail.com>
2022-05-31 13:26:49 +0530

    Roter has been added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Home from "./Pages/Home";
-// import Cart from "./Pages/Cart";
-// import Login from "./Pages/Login";
-// import Product from "./Pages/Product";
-// import ProductList from "./Pages/ProductList";
-// import Register from "./Pages/Register";
+import Cart from "./Pages/Cart";
+
+import Product from "./Pages/Product";
+import ProductList from "./Pages/ProductList";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
+
 const App = () => {
+  const user = true;
   return (
-    <div>
-      <Home />
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/products/:category" element={<ProductList />} />
+        <Route path="/product/:id" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
+        <Route
+          path="/register"
+          element={user ? <Navigate to="/" /> : <Register />}
+        />
+      </Routes>
+    </Router>
   );
 };
 
