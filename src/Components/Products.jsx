f810c6e48704577de6c811376569d05bd632b4b5
Ardhendu Pramanik <ardhendujj002@gmail.com>
2022-06-01 01:01:06 +0530

    sorting filtering features has been added after the data has been fatched from the database with the help of useEffect hooks and axios library

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -1,6 +1,8 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 import styled from "styled-components";
 import Product from "./Product";
-import { popularProducts } from "../data";
+// import { popularProducts } from "../data";
 
 const Container = styled.div`
   display: flex;
@@ -8,12 +10,56 @@ const Container = styled.div`
   flex-wrap: wrap;
   justify-content: space-between;
 `;
-const Products = () => {
+const Products = ({ cat, filters, sort }) => {
+  const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const res = await axios.get(
+          cat
+            ? `http://localhost:3000/api/products?category=${cat}`
+            : "http://localhost:3000/api/products"
+        );
+        setProducts(res.data);
+        // console.log(res);
+      } catch (err) {}
+    };
+    getProducts();
+  }, [cat]);
+
+  useEffect(() => {
+    cat &&
+      setFilteredProducts(
+        products.filter((item) =>
+          Object.entries(filters).every(([key, value]) =>
+            item[key].includes(value)
+          )
+        )
+      );
+  }, [products, cat, filters]);
+  useEffect(() => {
+    if (sort === "newest") {
+      setFilteredProducts((prev) =>
+        [...prev].sort((a, b) => a.createdAt - b.createdAt)
+      );
+    } else if (sort === "asc") {
+      setFilteredProducts((prev) =>
+        [...prev].sort((a, b) => a.price - b.price)
+      );
+    } else {
+      setFilteredProducts((prev) =>
+        [...prev].sort((a, b) => b.price - a.price)
+      );
+    }
+  }, [sort]);
+
   return (
     <Container>
-      {popularProducts.map((items) => (
-        <Product items={items} key={items.id} />
-      ))}
+      {cat
+        ? filteredProducts.map((item) => <Product item={item} key={item.id} />)
+        : products.map((item) => <Product item={item} key={item.id} />)}
     </Container>
   );
 };
