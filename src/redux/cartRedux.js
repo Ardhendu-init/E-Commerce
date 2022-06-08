96f94c0e3abb3cc3c48748613582a70903d1d68a
Ardhendu Pramanik <ardhendujj002@gmail.com>
2022-06-08 19:33:14 +0530

    The functionality of update and handling duplicate elemnet while adding to cart has been handled

--- src/redux/cartRedux.js
+++ src/redux/cartRedux.js
@@ -9,12 +9,49 @@ const cartSlice = createSlice({
   },
   reducers: {
     addProduct: (state, action) => {
-      state.quantity += 1; //cart quantity
-      state.products.push(action.payload);
-      state.total += action.payload.price * action.payload.quantity;
+      const foundItem = state.products.find((item) => {
+        if (item._id === action.payload.id) {
+          return item;
+        } else {
+          return false;
+        }
+      });
+      if (!foundItem) {
+        state.quantity += 1; //cart quantity
+        state.products.push(action.payload);
+        state.total += action.payload.price * action.payload.quantity;
+      } else {
+        foundItem.quantity = foundItem.quantity + 1;
+        state.total += foundItem.price;
+      }
+    },
+    updateProduct: (state, action) => {
+      state.products.map((item) => {
+        if (item._id === action.payload.id) {
+          if (action.payload.type === "inc") {
+            //if add button is pressed
+            item.quantity = item.quantity + 1;
+            state.total += item.price;
+          } else {
+            //for remove button
+
+            if (item.quantity === 1) {
+              // after removing if the quantity become zero we will remove the item from the cart
+              state.products = state.products.filter(
+                (i) => i._id !== action.payload.id
+              );
+              state.quantity -= 1;
+            } else {
+              item.quantity = item.quantity - 1;
+            }
+
+            state.total -= item.price;
+          }
+        }
+      });
     },
   },
 });
 
-export const { addProduct } = cartSlice.actions;
+export const { addProduct, updateProduct } = cartSlice.actions;
 export default cartSlice.reducer;
