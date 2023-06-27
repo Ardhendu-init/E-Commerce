e5ed96ebb894ad19706c87a4a0c23e756e2fe1c0
ardhendu_init_ <ardhendujj002@gmail.com>
2023-06-27 18:32:01 +0530

    Cyclic for backend hosting is used

--- src/requestMethod.js
+++ src/requestMethod.js
@@ -2,16 +2,17 @@ import axios from "axios";
 
 // const BASE_URL = "http://localhost:5000";
 // const HEROKU_URL = "https://stark-scrubland-62059.herokuapp.com";
-const renderUrl = "https://e-commerce-backend-eqts.onrender.com";
+// const renderUrl = "https://e-commerce-backend-eqts.onrender.com";
+const cyclicUrl = "https://witty-pink-snapper.cyclic.app";
 const TOKEN =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyOGY5Y2NhZTlmYzgyYjhmNDhhMzVkNCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY1NDA2NDQzNCwiZXhwIjoxNjU0MjM3MjM0fQ.oK4rzOXncrXDrAxeK8GAl-dwefnKhOlrKEHP3fcdke0";
 
 export const publicRequest = axios.create({
-  baseURL: renderUrl,
+  baseURL: cyclicUrl,
 });
 
 export const userRequest = axios.create({
-  baseURL: renderUrl,
+  baseURL: cyclicUrl,
   headers: {
     token: `Bearer ${TOKEN}`,
   },
