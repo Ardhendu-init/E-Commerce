1f79db436cb4455c02c4b13c99f42759fbc3d1dc
ardhendu_init_ <ardhendujj002@gmail.com>
2023-06-27 16:59:03 +0530

    Render hosting url added

--- src/requestMethod.js
+++ src/requestMethod.js
@@ -1,19 +1,18 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000";
+// const BASE_URL = "http://localhost:5000";
 // const HEROKU_URL = "https://stark-scrubland-62059.herokuapp.com";
+const renderUrl = "https://e-commerce-backend-eqts.onrender.com";
 const TOKEN =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyOGY5Y2NhZTlmYzgyYjhmNDhhMzVkNCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY1NDA2NDQzNCwiZXhwIjoxNjU0MjM3MjM0fQ.oK4rzOXncrXDrAxeK8GAl-dwefnKhOlrKEHP3fcdke0";
 
 export const publicRequest = axios.create({
-  baseURL: BASE_URL,
+  baseURL: renderUrl,
 });
 
 export const userRequest = axios.create({
-  baseURL: BASE_URL,
+  baseURL: renderUrl,
   headers: {
     token: `Bearer ${TOKEN}`,
   },
 });
-
-//Heroku url : https://stark-scrubland-62059.herokuapp.com/
